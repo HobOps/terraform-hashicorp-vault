497a76b96ec897906e26b1d269f62e0c7deb523b
Juan Carrillo <inetshell@gmail.com>
2022-12-04 18:16:03 -0600

    vault_mount: Added options var

--- modules/vault_mount/main.tf
+++ modules/vault_mount/main.tf
@@ -2,4 +2,5 @@ resource "vault_mount" "default" {
   path        = var.path
   type        = var.type
   description = var.description
+  options     = var.options
 }
--- vault_mount.tf
+++ vault_mount.tf
@@ -8,11 +8,17 @@ variable "type" {
   default = "kv-v2"
 }
 
+variable "options" {
+  type    = any
+  default = {}
+}
+
 module "vault_mount" {
   for_each = var.vault_mount != "" ? toset([var.vault_mount]) : []
   source   = "./modules/vault_mount/"
   path     = each.key
   type     = var.type
+  options  = var.options
 }
 
 output "vault_mount" {
