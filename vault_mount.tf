7045680f2f0ddac44996148268804b8abf8fac44
Juan Carrillo <inetshell@gmail.com>
2022-12-04 18:01:36 -0600

    vault_mount: Added description and type vars

--- vault_mount.tf
+++ vault_mount.tf
@@ -4,9 +4,11 @@ variable "vault_mount" {
 }
 
 module "vault_mount" {
-  for_each = var.vault_mount != "" ? toset([var.vault_mount]) : []
-  source   = "./modules/vault_mount/"
-  path     = each.key
+  for_each    = var.vault_mount != "" ? toset([var.vault_mount]) : []
+  source      = "./modules/vault_mount/"
+  path        = each.key
+  description = lookup(var.vault_mount, "description", "")
+  type        = lookup(var.vault_mount, "type", "")
 }
 
 output "vault_mount" {
